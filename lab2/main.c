5c57b0d9484fd25fbb21fc3b409be229c94a53e2
Хабибуллин Илья Русланович <HabibullinIR@main.sgu.ru>
2018-10-20 15:12:34 +0400

    unsuccesscful result

--- lab2/main.c
+++ lab2/main.c
@@ -2,16 +2,26 @@
 
 void wait(unsigned int mx){
 	int i;
-	for(i = 0;i<mx;i++){
-		
-	}
+	for(i = 0;i<mx;i++){}
 }
 #define delay 1000000
 
 void SystemInit(){
 	//RCC
 	RCC->AHB1ENR|=0x00000008;
+	RCC->APB1ENR |= 0x00000001;
 	GPIOD->MODER|=0x01000000;
+	
+	
+	TIM2->CNT = 0;
+	TIM2->PSC = 0x800;
+	TIM2->ARR = 0x8000;
+	TIM2->DIER |= 0x00000001;
+	TIM2->CR1 |= 0x00000001;
+	
+	NVIC->ISER[0] |= 0x10000000;
+	
+	
 }
 int main(){
 	while(1){
@@ -20,6 +30,4 @@ int main(){
 		GPIOD->ODR&=~0x00001000;
 		wait(delay);
 	}
-	
-
 }
